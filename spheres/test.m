313feb535569251102a4d91a106971a54df7e4f0
jaron <jaringson@gmail.com>
2018-03-22 21:55:20 -0600

    Start chapter 10 and spheres example

---
+++ spheres/test.m
@@ -0,0 +1,14 @@
+[X,Y,Z] = sphere;
+x = linspace(-1, 1, 75);
+y = -x.^2;
+figure(1)
+for k2 = 1:2
+    for k1 = 1:length(x)
+        surf(X+y(k1), Y+sin(2*pi*x(k1)), Z+cos(2*pi*x(k1)))
+        axis([-5  5    -5  5    -5  5])
+        axis square
+        view([-20  20])
+        refreshdata
+        drawnow
+    end
+end
